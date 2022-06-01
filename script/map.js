081fb29a11fb60e800ae6fcf6730a9a8f2947e9a
nicolasyvon <clain.nicolasyvon@gmail.com>
2022-06-01 10:52:51 +0200

    ouioui

--- script/map.js
+++ script/map.js
@@ -1,14 +1,54 @@
-window.addEventListener('DOMContentLoaded', ()=>{
-    // 1 : création
-let maCarte = L.map('carteFeu');
-
-    // 2 : choix du fond de carte
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-  attribution: '©️ OpenStreetMap contributors'
-}).addTo(maCarte);
-  
-    // 3 : réglage de la partie visible (centre, niveau de zoom)
-maCarte.setView([50.61, 3.14], 14);
-
-   // ...
+mapboxgl.accessToken = 'pk.eyJ1IjoiZmlyZWZvcmNlIiwiYSI6ImNsM3ZiYzB2bTB1ejQzanJ4dWJudjg2MjgifQ.R0qQLerxhp73rgRAVG6nSw';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/satellite-v9'
 });
+map.on('load', function() {
+   
+    // … ajout source
+ 
+    // … ajout layers
+ 
+    const bounds = [
+      [-6.494917884024034, 51.69935897822677],  // Coin Sud-Ouest
+      [9.732690059356145, 41.25166002712723]    // Coin Nord-Est
+    ];
+ 
+    map.fitBounds(bounds);
+ });
+ map.on('load', function() {
+   
+    // … ajout source
+ 
+    // … ajout layers
+ 
+    // … centrer la carte
+ 
+    const paris = {
+       "geometry": {
+         "coordinates": [
+            2.350773675310819,
+            48.86015558292965
+          ],
+         "type": "Point"
+       },
+       "type": "Feature",
+       "properties": {
+         "name": "Paris"
+       }
+    };
+ 
+   map.addSource('paris_point',
+                 { 'type': 'geojson', 'data': paris }
+    );
+ 
+ 
+    map.addLayer({
+             'id': 'paris_layer_name',
+             'type': 'symbol',
+             'source': 'paris_point',
+             'layout': {
+               'text-field': ['get', 'name']
+             }
+         });
+ });
