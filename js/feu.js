a2fad7d134296f3344f6c53468e0c111e28e5ac8
Amandine <amandine.piccinali@gmail.com>
2022-06-01 11:51:32 +0200

    daccorrrd

--- js/feu.js
+++ js/feu.js
@@ -6,3 +6,16 @@ async function afficheFeu() {
     const responseText = await response.text();    
     console.log(responseText); // logs 'OK'
 }
+
+
+function RecupFeu (){
+
+    var Lfeu = [{"id":57,"type":"C_Flammable_Gases","intensity":50.0,"range":50.0,"lon":4.8260937761478795,"lat":45.732333858926715},
+                {"id":59,"type":"B_Gasoline","intensity":50.0,"range":50.0,"lon":4.808583763718545,"lat":45.793118996773316}];
+
+
+    for (i=0;i<Lfeu.length;i++){
+        var nom = "Feu" + String(i);
+        console.log(nom);
+    }
+}
