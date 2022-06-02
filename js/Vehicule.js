0e1db954b353bd9881b7d75c07aac7ee1f820029
Amandine <amandine.piccinali@gmail.com>
2022-06-02 08:55:49 +0200

    ajoutmarchemaispasdelete

---
+++ js/Vehicule.js
@@ -0,0 +1,41 @@
+async function creationVehicule() {
+
+    const object = {
+        lon : 4.808583763718546,
+        lat : 45.793118996773314,
+        type : "CAR",
+        liquidType : "ALL",
+        liquidQuantity : 30,
+        fuel : 100,
+        crewMember : 0,
+        facilityRefID : 86
+        //attack: document.querySelector("input[name='attack']").value
+    }
+
+    ;
+
+    const response = await fetch('http://vps.cpe-sn.fr:8081/vehicle/10eec12c-ac37-4479-b4a8-707559a6ee62', {
+                                method: 'POST',     
+                                body: JSON.stringify(object),
+                                headers: {
+                                     'Content-Type': 'application/json'
+                                    }
+                                });
+    const responseText = await response.text();
+
+    console.log(responseText); // logs 'OK'
+}
+
+
+async function SuppressionVehicules() {
+
+    var lien = 'http://vps.cpe-sn.fr:8081/vehicle/10eec12c-ac37-4479-b4a8-707559a6ee62';
+
+
+    const response = await fetch(lien, {
+                                method: 'DELETE',
+                                });
+    const responseText = await response.text();
+
+    console.log(responseText); // logs 'OK'
+}
--- js/feu.js
+++ js/feu.js
@@ -23,6 +23,9 @@ async function RecupFeu() {
     return Lfeu;
 }
 
+
+
+
 //[{"id":59,"type":"B_Gasoline","intensity":50.0,"range":50.0,"lon":4.808583763718545,"lat":45.793118996773316},{"id":57,"type":"C_Flammable_Gases","intensity":50.0,"range":50.0,"lon":4.8260937761478795,"lat":45.732333858926715}]
 
 
@@ -55,51 +58,5 @@ async function afficheFeu(){
                 .setPopup(popup)
                 .addTo(map);
     }
-
-    //On garde parce qu'on a le seum d'avoir fait tout ca pour rien
-    /*
-    map.on('load', function() {
-
-        for (i=0;i<Lfeu.length;i++){
-            var nom = "Feu" + String(i);
-    
-            console.log("rentre dans le for");
-
-            var fire = {
-                "geometry": {
-                    "coordinates": [
-                        Lfeu[i]["lon"], //longitude
-                        Lfeu[i]["lat"] //latitude
-                    ],
-                    "type": "Point"
-                },
-                "type": "Feature",
-                "properties": {
-                    "name": nom
-                }
-            };
-
-            map.addSource(nom+'_point',
-                        { 'type': 'geojson', 'data': fire }
-            );
-
-
-            map.addLayer({
-                'id': nom+'_layer_name',
-                'type': 'symbol',
-                'source': nom+'_point',
-                'layout': {
-                    'text-field': ['get', 'name']
-                }
-        
-            });
-
-            fire = {};
-            
-            console.log(Lfeu);
-        }
-    
-    }); 
-    */ 
     
 }
